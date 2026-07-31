5de67daffa957cffe38d36eccdaaeb4f7c7e8224
bharambepratik08-star <bharambepratik08@gmail.com>
2026-07-31 23:38:13 +0530

    In Progress

---
+++ JS/responsive.js
@@ -0,0 +1,8 @@
+const navBtn_respui = document.querySelector('.navbar-Btn');
+const main_Navbar = document.querySelector('.navBar-header');
+const navbar_bnt = document.querySelector('.navBar_respoUi_btn');
+
+navbar_bnt.addEventListener("click", () => {
+    navBtn_respui.style.display = 'none';
+    main_Navbar.style.display = 'flex'
+})
